95fdad142650ff87244015798f8e50c07ba6cb15
HyeongTaek Jo <diajd169@gmail.com>
2024-11-28 13:55:06 +0900

    dd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // 모든 네트워크 인터페이스에서 접근 가능
-    //port: 50,      // 원하는 포트로 변경
+    port: 80,      // 원하는 포트로 변경
     // proxy: {
     //   '/api': 'http://140.245.65.135:5000',  // 프론트엔드에서 /api 요청을 배포된 백엔드로 프록시합니다.
     // }
