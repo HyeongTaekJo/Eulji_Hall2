d6ef8019c6c4ac25569a93ec0a013f12815d47be
HyeongTaek Jo <diajd169@gmail.com>
2024-11-28 11:08:25 +0900

    dd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
         target: 'http://localhost:5000', // API 서버
         changeOrigin: true, // 호스트 헤더 변경
         // rewrite: (path) =>  path.replace(/^\/api/, ''), // '/api'를 제거
-        rewrite: (path) => path.replace(/^\/api/, ''), // '/api'를 제거
+        //rewrite: (path) => path.replace(/^\/api/, ''), // '/api'를 제거
       },
       // '^/[^/].*': { // '/'가 아닌 '/'뒤에 반드시 문자열이 붙은 정규표현식
       //   target: 'http://localhost:5000', // API 서버
