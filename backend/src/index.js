1d1e1fd7ba2c622112f308cf28686dd19ad2b65a
HyeongTaek Jo <diajd169@gmail.com>
2024-11-28 16:52:34 +0900

    ㅇㅇ

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,6 +16,7 @@ const corsOptions = {
     //origin: '*', //프론트엔드 3000요청 허용,
     origin: [
              'http://140.245.65.135',
+             'http://www.hallreservation.site'
             ], // 허용할 출처
     methods: ["GET", "POST", "PUT", "DELETE"],
     //allowedHeaders: ["Content-Type", "Authorization"], // 허용할 헤더
