e3aba3a7ef6c5db6abb487dcd43b20dac7d21533
HyeongTaek Jo <diajd169@gmail.com>
2024-12-05 16:15:43 +0900

    dd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // 모든 네트워크 인터페이스에서 접근 가능
-    port: 80,      // 원하는 포트로 변경
+    //host: '0.0.0.0', // 모든 네트워크 인터페이스에서 접근 가능
+    //port: 80,      // 원하는 포트로 변경
 
     //프록시는 처리하지 않는 것이 좋다.
     //어차피 cors로 잡아주기 때문에 또 할필요 없고 오류가 계속 발생한다.
