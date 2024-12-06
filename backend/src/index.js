2972eb0f0d42531f95063eb85b6f6bc047e2206d
HyeongTaek Jo <diajd169@gmail.com>
2024-12-06 14:51:31 +0900

    dd

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,8 +15,9 @@ const corsOptions = {
     //프록시 처리하지 말 것 오류남
     // origin: '*', //프론트엔드 3000요청 허용,
     origin: [
-            //  'http://140.245.65.135',
-             'http://localhost:5000',
+             "http://140.245.65.135",
+             "http://localhost:5000",
+             "http://localhost",
             ], // 허용할 출처
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"], // 허용할 헤더
